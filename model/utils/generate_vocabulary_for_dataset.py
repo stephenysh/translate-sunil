8a5445151eaa19576870d652ec50303433d1228e
ZackJiang21 <zackjiang@foxmail.com>
2020-02-21 15:35:43 +0400

    [Requirement] Update requirements.txt

--- model/utils/generate_vocabulary_for_dataset.py
+++ model/utils/generate_vocabulary_for_dataset.py
@@ -1,32 +1,24 @@
-
-
-
-import numpy as np
+import copy
 import os
 import pickle
-import pandas as pd
-import re
 import random
 
-import copy
-
+import numpy as np
 
 
 class KnownWordCharacter:
-    def __init__(self,pretrained_word_dict, wlowered, clowered):
+    def __init__(self, pretrained_word_dict, wlowered, clowered):
 
         self.index2word = {0: "PADDING", 1: "RE_DIGITS", 2: "UNKNOWN"}
         self.word2index = {"RE_DIGITS": 1, "UNKNOWN": 2, "PADDING": 0}
-
 
         self.word2count = {"RE_DIGITS": 1, "UNKNOWN": 1, "PADDING": 1}
         self.ori_n_words = 3  # Count SOS and EOS
         self.ori_word2index = {"RE_DIGITS": 1, "UNKNOWN": 2, "PADDING": 0}
         self.ori_index2word = {0: "PADDING", 1: "RE_DIGITS", 2: "UNKNOWN"}
 
-
-        self.char2index = {'P_':0, 'U_':1}
-        self.index2char = {0: 'P_', 1:'U_'}
+        self.char2index = {'P_': 0, 'U_': 1}
+        self.index2char = {0: 'P_', 1: 'U_'}
         self.char2count = {'P_': 1, 'U_': 1}
 
         self.n_chars = 2
@@ -57,18 +49,14 @@ class KnownWordCharacter:
         else:
             self.word2count['UNKNOWN'] += 1
 
-
-
         # each word should be parsed into characters
         if self.clowered == True:
             pre_char_word = word.lower()
         else:
             pre_char_word = word
 
-
         for cur_char in pre_char_word:
             self.addChar(cur_char)
-
 
     def addChar(self, character):
         if character not in self.char2index:
@@ -80,18 +68,16 @@ class KnownWordCharacter:
 
             self.char2count[character] += 1
 
-
     def filtering(self, minWFreq, minChFreq):
         loopN = 3
         for i in range(3, len(self.ori_index2word)):
-            cur_w  = self.ori_index2word[i]
+            cur_w = self.ori_index2word[i]
             cur_F = self.word2count[cur_w]
 
             if cur_F >= minWFreq:
                 self.index2word[loopN] = cur_w
                 self.word2index[cur_w] = loopN
                 loopN += 1
-
 
         org_index2char = copy.deepcopy(self.index2char)
 
@@ -109,42 +95,32 @@ class KnownWordCharacter:
                 loopN += 1
 
 
-
 def generate_vocabulary_save_embeddings(lowF, txtfile, w_dict, v_matrix, save_p):
-
     sent_array = []
     with open(txtfile, 'r', encoding='utf-8') as lines:
         for line in lines:
             sent_array.append(line.strip('\n'))
 
-
-
-    Word_Cha_Class = KnownWordCharacter(w_dict, wlowered = True, clowered = True)
+    Word_Cha_Class = KnownWordCharacter(w_dict, wlowered=True, clowered=True)
 
     for cur_s in sent_array:
         Word_Cha_Class.addSentence(cur_s)
 
+    print('No Unique Characters:', Word_Cha_Class.n_chars)
+    print('No Unique Known Words:', Word_Cha_Class.ori_n_words)
 
-
-    print('No Unique Characters:',Word_Cha_Class.n_chars)
-    print('No Unique Known Words:',Word_Cha_Class.ori_n_words)
-
-
-    Word_Cha_Class.filtering(minWFreq = lowF, minChFreq =1)
+    Word_Cha_Class.filtering(minWFreq=lowF, minChFreq=1)
 
     print('No unique known words after filtering', len(Word_Cha_Class.index2word))
-
 
     final_w_size = len(Word_Cha_Class.index2word)
     final_cha_size = len(Word_Cha_Class.index2char)
 
     # save vocabulary
-    with open(os.path.join(save_p, 'Word_Cha_Class_%d_%d.pickle'%(final_w_size, final_cha_size)), 'wb') as f:
+    with open(os.path.join(save_p, 'Word_Cha_Class_%d_%d.pickle' % (final_w_size, final_cha_size)), 'wb') as f:
         pickle.dump(Word_Cha_Class, f)
 
-
     # save vectors
-
 
     GLOVE_VECTORS = pickle.load(open(v_matrix, 'rb'))
     GLOVE_WORDS = pickle.load(open(w_dict, "rb"))
@@ -175,20 +151,16 @@ def generate_vocabulary_save_embeddings(lowF, txtfile, w_dict, v_matrix, save_p)
     print('word size:', len(Word_Cha_Class.index2word))
     print('char size:', len(Word_Cha_Class.index2char))
 
-    with open(os.path.join(save_p, 'save_initializaton_embeddings_%d_%d.pickle'%(final_w_size, final_cha_size)), 'wb') as f:
+    with open(os.path.join(save_p, 'save_initializaton_embeddings_%d_%d.pickle' % (final_w_size, final_cha_size)),
+              'wb') as f:
         pickle.dump(S_W_Matrix, f)
 
 
-
-
-
-
 if __name__ == '__main__':
-
-
     input_text = '/home/jl1/MyIIAIData/FewShotNERData/input/cross_domain_cross_type/ontonotes2fgner/xx.txt'
     input_embedding_Word_dict = '/home/jl1/MyIIAIData/glove/glove_words.pickle'
     input_embedding_Vector_matrix = '/home/jl1/MyIIAIData/glove/glove_vectors.pickle'
     save_path = '/home/jl1/MyIIAIData/FewShotNERData/input/cross_domain_cross_type/xxx'
-    #Word_Cha_Class = KnownWordCharacter(w_dict, wlowered=True, clowered=False)
-    generate_vocabulary_save_embeddings(3,input_text, input_embedding_Word_dict, input_embedding_Vector_matrix, save_path)
+    # Word_Cha_Class = KnownWordCharacter(w_dict, wlowered=True, clowered=False)
+    generate_vocabulary_save_embeddings(3, input_text, input_embedding_Word_dict, input_embedding_Vector_matrix,
+                                        save_path)
